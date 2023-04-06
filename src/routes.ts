bd233aa9ac67626db239bc93d7336fd15e3296eb
caio lucena <caio.lcolaco@gmail.com>
2023-04-06 12:24:55 -0300

    added a login route

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 const routes = express.Router()
 
-import { createUser, updateUser, deleteUser, findUser, findAllUsers, findFilterUsers } from './controllers/UserControllers';
+import { createUser, updateUser, deleteUser, findUser, findAllUsers, findFilterUsers, authenticationUser } from './controllers/UserControllers';
 
 // User Routes
 routes.get('/users', findAllUsers)
 routes.get('/user/:id', findUser)
 routes.get('/filterUsers', findFilterUsers)
-routes.post('/user', createUser)
+routes.post('/auth/registerUser', createUser)
+routes.post('/auth/loginUser', authenticationUser)
 routes.put('/user/:id', updateUser)
 routes.delete('/user/:id', deleteUser)
 
