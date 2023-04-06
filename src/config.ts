7f1f5191a6c2691f9bb978926e8d5fddbb51d7aa
caio lucena <caio.lcolaco@gmail.com>
2023-04-06 12:23:36 -0300

    added a new env to secretkey

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const PORT = process.env.PORT
+const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
 
-export { PORT }
+export { PORT, AUTH_SECRET_KEY }
