e6dab0220011ab5e67114c38a75ab9d9e1ed5008
caio lucena <caio.lcolaco@gmail.com>
2023-04-06 16:44:17 -0300

    added process routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from 'express'
 const routes = express.Router()
 
 import { createUser, updateUser, deleteUser, findUser, findAllUsers, findFilterUsers, authenticationUser } from './controllers/UserControllers';
+import { createProcess, updateProcess, deleteProcess, findProcess, findAllProcess, findFilterProcess } from './controllers/ProcessControllers';
 import { checkToken } from './middlewares/checkToken';
 
 // User Routes
@@ -12,5 +13,13 @@ routes.post('/auth/registerUser', createUser)
 routes.post('/auth/loginUser', authenticationUser)
 routes.put('/user/:id', updateUser)
 routes.delete('/user/:id', deleteUser)
+
+// Process Routes
+routes.get('/process', findAllProcess)
+routes.get('/process/:id', findProcess)
+routes.get('/filterProcess', findFilterProcess)
+routes.post('/process', createProcess)
+routes.put('/process/:id', updateProcess)
+routes.delete('/process/:id', deleteProcess)
 
 export default routes;
